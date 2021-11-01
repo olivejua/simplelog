ad6ff828208293726e48f0f629cd12e094e9396b
olivejua <tmfrl4710@gmail.com>
2021-11-01 14:12:55 +0900

    :white_check_mark: test: restDocs snippet 생성 로직 추가 (#11)
    추가한 내용
    - RestDocsConfiguration class 생성: snippet 출력시 읽기 좋은 형식으로 바꿔주는 설정 추가
    - IntegrationTest에 RestDocs 컴포넌트 추가 및 AutoConfigureRestDocs: 테스트 후 자동으로 snippet 생성해줄 수 있는 설정 추가
    - ObjectMapper 필드 추가: json 전송할 때 직렬화해줄 수 있는 객체

--- src/test/java/com/simplelog/api/integration/IntegrationTest.java
+++ src/test/java/com/simplelog/api/integration/IntegrationTest.java
@@ -1,19 +1,24 @@
 package com.simplelog.api.integration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.simplelog.api.common.docs.RestDocsConfiguration;
 import com.simplelog.api.common.factory.PostFactory;
 import com.simplelog.api.common.factory.UserFactory;
 
 @SpringBootTest
 @Transactional
+@AutoConfigureRestDocs
 @AutoConfigureMockMvc
 @Import({
+	RestDocsConfiguration.class,
 	UserFactory.class,
 	PostFactory.class
 })
@@ -21,6 +26,9 @@ public class IntegrationTest {
 
 	@Autowired
 	protected MockMvc mockMvc;
+
+	@Autowired
+	protected ObjectMapper objectMapper;
 
 	@Autowired
 	protected UserFactory userFactory;
