c433a82f212bef754fbd64aeb25b44e2cafd0b88
viiviii <viiiix.viiviii@gmail.com>
2021-12-20 20:16:32 +0900

    :recycle: refactor: CommentReplyContent로 Comment content 유효성 검증 분리 (#16)

--- src/main/java/com/simplelog/api/domain/Comment.java
+++ src/main/java/com/simplelog/api/domain/Comment.java
@@ -22,23 +22,16 @@ public class Comment extends BaseTimeEntity {
     @JoinColumn(name = "post_id", nullable = false)
     private Post post;
 
-    @Column(nullable = false, length = 500)
-    private String content;
+    @Embedded
+    private CommentReplyContent content;
 
-    public Comment(User user, Post post, String content) {
-        if (invalid(content)) {
-            throw new IllegalArgumentException();
-        }
+    public Comment(User user, Post post, CommentReplyContent content) {
         this.user = user;
         this.post = post;
         this.content = content;
     }
 
-    private boolean invalid(String content) {
-        return content == null || content.isEmpty() || content.length() > 500;
-    }
-
     public String getContent() {
-        return content;
+        return content.get();
     }
 }
---
+++ src/main/java/com/simplelog/api/domain/CommentReplyContent.java
@@ -0,0 +1,46 @@
+package com.simplelog.api.domain;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CommentReplyContent {
+
+    static final int MAX_LENGTH = 500;
+
+    @Column(nullable = false, length = MAX_LENGTH)
+    private String content;
+
+    public CommentReplyContent(String content) {
+        if (invalid(content)) {
+            throw new IllegalArgumentException();
+        }
+        this.content = content;
+    }
+
+    private boolean invalid(String content) {
+        return content == null || content.isEmpty() || content.length() > MAX_LENGTH;
+    }
+
+    public String get() {
+        return content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CommentReplyContent)) return false;
+        CommentReplyContent that = (CommentReplyContent) o;
+        return content.equals(that.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(content);
+    }
+}
